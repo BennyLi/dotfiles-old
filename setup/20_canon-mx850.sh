442fecb480095b706e945acf2c60c6c3258eee36
Benjamin Lindemann <benjamin.lindemann@iteratec.de>
2016-11-23 14:24:01 +0100

    Addition group needed to use the cups web admin panel

--- setup/20_canon-mx850.sh
+++ setup/20_canon-mx850.sh
@@ -3,6 +3,7 @@
 sudo pacman -S cups gutenprint
 yaourt -S cups-bjnp
 sudo gpasswd -a $USER lp
+sudo gpasswd -a $USER sys
 
 sudo systemctl enable cups-browsed.service
 sudo systemctl start cups-browsed.service
